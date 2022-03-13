2575408ff80a320d9ef35f678ab208ccde16a856
Nec2005 <necatiunal2005@gmail.com>
2022-03-13 11:39:53 +0000

    weekly-qa

--- jenkins/build-and-deploy-petclinic-on-qa-env-manually.sh
+++ jenkins/build-and-deploy-petclinic-on-qa-env-manually.sh
@@ -1,7 +1,7 @@
 PATH="$PATH:/usr/local/bin"
 APP_NAME="petclinic"
 APP_REPO_NAME="test-repo/petclinic-app-qa"
-APP_STACK_NAME="test-petclinic-App-QA-1"
+APP_STACK_NAME="test-petclinic-App-QA-3"
 CFN_KEYPAIR="test-petclinic-qa.key"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 AWS_REGION="us-east-1"
